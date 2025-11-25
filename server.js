cfac3da7e903a6a216c381cac848379576bb3934
Hasnain Tanoli <hasnaintanoli.official@gmail.com>
2025-11-25 10:50:03 +0500

    feat: Introduce Swiss-Bento design system with new home and dashboard UI, update server port, and clear .env example placeholders.

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use("/", router);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8001;
   app.listen(PORT, () => {
     console.log(`Server Started on PORT: ${PORT}`);
   });
